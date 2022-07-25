93e8ede7651eaa1ae89f62713f42e4b73da63bae
doongidoong <wlgns3398@naver.com>
2022-07-26 02:12:54 +0900

    Refactor : social login, controller service 구분

--- src/main/java/com/speakeasy/service/SignService.java
+++ src/main/java/com/speakeasy/service/SignService.java
@@ -1,25 +1,58 @@
 package com.speakeasy.service;
 
 import com.speakeasy.domain.User;
+import com.speakeasy.exception.EmailSigninFailedException;
 import com.speakeasy.exception.UserNotFoundException;
 import com.speakeasy.repository.UserRepository;
+import com.speakeasy.request.UserSignIn;
+import com.speakeasy.request.UserSignUp;
 import com.speakeasy.response.KakaoProfile;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Slf4j
-@Service
+import java.util.Collections;
+import java.util.Optional;
+
 @RequiredArgsConstructor
-public class SignService {
+@Service
+public class UserService implements UserDetailsService {
+
     private final UserRepository userRepository;
-    //lombok을 통해 requireArg로 생성자로 만듦
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    private final KakaoService kakaoService;
 
 
-    public void join(User user){
-        // postCreate(request)-> Entity
+    public UserDetails loadUserByUsername(String userPk) {
+        return userRepository.findById(Long.valueOf(userPk)).orElseThrow(UserNotFoundException::new);
+    }
+    //쉽게 말하면 UserDetails 인터페이스는 VO 역할
+
+    public void join(UserSignUp request){
+        User user= User.builder()
+                .uid(request.getUid())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .name(request.getName())
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build();
         userRepository.save(user);
     }
 
+    public User comparePassword(UserSignIn request){
+        User user = userRepository.findByUid(request.getUid()).orElseThrow(EmailSigninFailedException::new);
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new EmailSigninFailedException();
+        }
+        return user;
+    }
 
+    public Optional<User> kakaoLogin(String provider, KakaoProfile profile){
+        Optional<User> user = userRepository.findByUidAndProvider(String.valueOf(profile.getId()), provider);
+        return user;
+    }
 }
+
